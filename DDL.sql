ad14fc4956670db63ff1002852e0a681696e87e3
Guilherme Migliano <guilhermemigliano@iMac-de-Guilherme.local>
2021-03-16 20:57:19 -0300

    Commit inicial

---
+++ DDL.sql
@@ -0,0 +1,28 @@
+create schema anotacao;
+
+use anotacao;
+
+create user 'user'@'localhost' identified by 'pass123';
+
+grant select, insert, delete, update on anotacao.* to user@'localhost';
+
+create table usr_usuario (
+usr_id bigint unsigned not null auto_increment, usr_nome varchar(20) not null,
+usr_email varchar(100) not null,
+usr_senha varchar(100) not null,
+primary key (usr_id),
+unique key uni_usuario_nome (usr_nome),
+unique key uni_usuario_email (usr_email)
+);
+create table aut_autorizacao (
+aut_id bigint unsigned not null auto_increment, aut_nome varchar(20) not null,
+primary key (aut_id),
+unique key uni_aut_nome (aut_nome)
+);
+
+create table uau_usuario_autorizacao ( usr_id bigint unsigned not null, aut_id bigint unsigned not null, primary key (usr_id, aut_id), foreign key uau_usr_fk (usr_id)
+references usr_usuario (usr_id)
+on delete restrict on update cascade, foreign key uau_aut_fk (aut_id)
+references aut_autorizacao (aut_id)
+    on delete restrict on update cascade
+);
