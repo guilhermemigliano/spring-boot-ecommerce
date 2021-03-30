b7b65e24c13fbea5d137292fe01899402f12d77b
guimigliano <guimigliano@gmail.com>
2021-03-30 11:41:52 +0000

    Atualização Pedido Controller

--- src/main/java/br/gov/sp/fatec/ecommerce/controller/PedidoController.java
+++ src/main/java/br/gov/sp/fatec/ecommerce/controller/PedidoController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonView;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.gov.sp.fatec.ecommerce.entity.Pedido;
 import br.gov.sp.fatec.ecommerce.service.SegurancaService;
@@ -47,9 +50,17 @@ public class PedidoController {
     }
 
     @PostMapping
-    public Pedido cadastrarNovoPedido(@RequestBody Pedido pedido) throws Exception {
-        return segurancaService.criarPedido(pedido.getNome(), pedido.getValor(), "pedro@fatec.com.br");
-    }
+    public ResponseEntity<Pedido> cadastrarNovoPedido(@RequestBody Pedido pedido,
+    UriComponentsBuilder uriComponentsBuilder) throws Exception {
+        pedido = segurancaService.criarPedido(pedido.getNome(), pedido.getValor(), "pedro@fatec.com.br");
+        HttpHeaders responseHeaders = new HttpHeaders();
+            responseHeaders.setLocation(
+                uriComponentsBuilder.path("/cliente/id/" + pedido.getId()).build().toUri());
+        
+        return new ResponseEntity<Pedido>(pedido, responseHeaders, HttpStatus.CREATED);
+    }   
    
+
+    
     
 }
