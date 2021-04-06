219eecded30f26980f9b69907847c0e6f2b25531
guimigliano <guimigliano@gmail.com>
2021-04-06 13:53:40 +0000

    Controller CrossOrigin e PutMapping Pedido

--- src/main/java/br/gov/sp/fatec/ecommerce/controller/PedidoController.java
+++ src/main/java/br/gov/sp/fatec/ecommerce/controller/PedidoController.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,9 +28,9 @@ import br.gov.sp.fatec.ecommerce.service.SegurancaService;
 
 //metodos que estiverem aqui podem ser rotas (serviços)
 @RestController
-//permite acesso externo de qualquer lugar se não colocar as origens. 
+// permite acesso externo de qualquer lugar se não colocar as origens.
 @RequestMapping(value = "/pedido")
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PedidoController {
 
     @Autowired
@@ -39,42 +41,63 @@ public class PedidoController {
 
     @JsonView(View.PedidoLista.class)
     @GetMapping
-    public List<Pedido> buscarClientes(){
+    public List<Pedido> buscarClientes() {
         return segurancaService.listarPedidos();
     }
 
     @JsonView(View.PedidoLista.class)
     @GetMapping(value = "/nome")
-    public Pedido buscarPedidoPorNome(@RequestParam(value = "nome") String nome){
+    public Pedido buscarPedidoPorNome(@RequestParam(value = "nome") String nome) {
         return segurancaService.buscarPedidoPorNome(nome);
     }
 
     @JsonView(View.PedidoLista.class)
     @GetMapping(value = "/id/{id}")
-    public Pedido buscarPedidoPorId(@PathVariable("id") Long id){
+    public Pedido buscarPedidoPorId(@PathVariable("id") Long id) {
         return segurancaService.buscarPedidoPorId(id);
     }
 
     @PostMapping
     public ResponseEntity<Pedido> cadastrarNovoPedido(@RequestBody Pedido pedido,
-    UriComponentsBuilder uriComponentsBuilder) throws Exception {
+            UriComponentsBuilder uriComponentsBuilder) throws Exception {
         pedido = segurancaService.criarPedido(pedido.getNome(), pedido.getValor(), "pedro@fatec.com.br");
         HttpHeaders responseHeaders = new HttpHeaders();
-            responseHeaders.setLocation(
-                uriComponentsBuilder.path("/pedido/id/" + pedido.getId()).build().toUri());
-        
+        responseHeaders.setLocation(uriComponentsBuilder.path("/pedido/id/" + pedido.getId()).build().toUri());
+
         return new ResponseEntity<Pedido>(pedido, responseHeaders, HttpStatus.CREATED);
-    }   
+    }
 
     @DeleteMapping("/id/{id}")
     public String deletePedido(@PathVariable(value = "id") Long id) {
         Pedido pedido = pedidoRepo.buscaPedidoPorId(id);
-        if(pedido != null){
+        if (pedido != null) {
             pedidoRepo.delete(pedido);
             return "Pedido excluído com sucesso";
-        }               
-        throw new RegistroNaoEncontradoException("Pedido não encontrado!");            
+        }
+        throw new RegistroNaoEncontradoException("Pedido não encontrado!");
     }
+
+    @PutMapping("/id/{id}")
+    public ResponseEntity<Pedido> atualizarPedido(@PathVariable(value = "id") Long id,
+            @Validated @RequestBody Pedido pedido) throws Exception {
+                pedido = pedidoRepo.buscaPedidoPorId(id);
+                if (pedido != null) {
+                    pedido.setValor(1150);
+                    pedidoRepo.save(pedido);
+
+                    return ResponseEntity.ok(pedido);
+
+                }
+
+                throw new RegistroNaoEncontradoException("Pedido não encontrado!");
+                
+        }
+
+        
+        
+            
+                
+    
 
     
    
