f5fc5cd16b95b7c1d7890a5968cc876a36413511
guilhermemigliano <guilhermemigliano@gmail.com>
2021-05-30 18:56:31 +0000

    Atualização ClienteRepo

--- src/main/java/br/gov/sp/fatec/ecommerce/entity/Cliente.java
+++ src/main/java/br/gov/sp/fatec/ecommerce/entity/Cliente.java
@@ -32,7 +32,7 @@ public class Cliente {
     @Column(name = "cli_nome")
     private String nome;
 
-    @JsonView({View.ClienteCompleto.class, View.PedidoLista.class})
+    @JsonView({View.ClienteResumo.class, View.ClienteCompleto.class, View.PedidoLista.class})
     @Column(name = "cli_email")
     private String email;
 
